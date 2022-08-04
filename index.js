c6a824fb7ac5517331a13a9f707f46551813402b
maxischiavello <maxischiavello12@gmail.com>
2022-08-04 14:04:16 -0300

    agregue models/ y routes/

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose= require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
         .then( ()=> console.log("DBconnection successfull"))
         .catch( (error) => console.log(error)
-    );
-    
+);
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log("backend server is running!")
