480787036aa1282a71a2de9c6118d7c94701f891
maxischiavello <maxischiavello12@gmail.com>
2022-08-05 13:33:18 -0300

    JWT, verificarToken y modificaciones

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register" , async (req, res) =>{
@@ -38,9 +39,18 @@ router.post("/login", async (req, res) =>{
         
         if (originalPassword !== req.body.password) res.status(401).json("Wrong credentials!");
         
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdming: user.isAdmin
+            },
+            process.env.JWT_SEC,
+            {expiresIn: "3d"}
+        );
+
         const { password, ...others} = user._doc;   
 
-        res.status(200).json(others);
+        res.status(200).json({ ...others, accessToken });
     } catch(error) {
         res.status(500).json(error);
     }
