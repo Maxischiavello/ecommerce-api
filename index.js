bd5f395277d1df5d7dbb3c9239f67985067278e5
maxischiavello <maxischiavello12@gmail.com>
2022-08-11 16:58:09 -0300

    agrego modulo stripe al index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log("backend server is running!")
